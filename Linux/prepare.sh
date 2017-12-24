b5ad3c8e6b48e2cf57d6fefb23a3fcd1091af821
DanielChabrowski <dantezstudio@gmail.com>
2017-12-24 22:58:35 +0100

    Add valgrind and change tabs to spaces

--- Linux/prepare.sh
+++ Linux/prepare.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 sudo apt-get update \
-	&& sudo apt-get install -y --no-install-recommends \
-	wget \
-	tree \
-	apt-transport-https \
-	build-essential \
-	git \
-	cmake \
-	clang-format-5.0 \
-	clang-tidy-5.0
+    && sudo apt-get install -y --no-install-recommends \
+    wget \
+    tree \
+    apt-transport-https \
+    build-essential \
+    git \
+    cmake \
+    clang-format-5.0 \
+    clang-tidy-5.0 \
+    valgrind
 
 # Install sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update \
-	&& sudo apt-get install -y --no-install-recommends \
-	sublime-text
+    && sudo apt-get install -y --no-install-recommends \
+    sublime-text
 
 # Use sublime-text as default editor for git
 git config --global core.editor "subl -n -w"
