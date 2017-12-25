24d89615794d78ea510e984120c3ef14dcf35737
DanielChabrowski <dantezstudio@gmail.com>
2017-12-25 17:19:02 +0100

    Add .vimrc install

--- Linux/prepare.sh
+++ Linux/prepare.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update \
     && sudo apt-get install -y --no-install-recommends \
+    vim \
     wget \
     tree \
     apt-transport-https \
@@ -11,6 +12,9 @@ sudo apt-get update \
     clang-format-5.0 \
     clang-tidy-5.0 \
     valgrind
+
+# Install vimrc
+cp .vimrc ~/.vimrc
 
 # Install sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
