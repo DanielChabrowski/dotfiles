a264eb2962c96d361814db4baa4ac3dda246de56
Daniel Chabrowski <dantezstudio@gmail.com>
2018-02-23 00:50:51 +0100

    Add tmux plugins and config

--- Linux/prepare.sh
+++ Linux/prepare.sh
@@ -29,6 +29,12 @@ cp -R powerline ~/.config/powerline
 # Install .bashrc file
 cp .bashrc ~/.bashrc
 
+# Install tmux plugins
+./tmux/install.sh
+
+# Install tmux config
+cp tmux/.tmux.conf ~/.tmux.conf
+
 # Install sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
