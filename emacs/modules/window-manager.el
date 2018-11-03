353a947875dc22a0682b1a03d30aa0672cc20030
Daniel Chabrowski <dantezstudio@gmail.com>
2018-11-03 12:46:09 +0100

    Remove emacs configuration

--- emacs/modules/window-manager.el
+++
@@ -1,8 +0,0 @@
-(require 'windmove)
-
-(global-set-key (kbd "C-x <left>") 'windmove-left)
-(global-set-key (kbd "C-x <right>") 'windmove-right)
-(global-set-key (kbd "C-x <up>") 'windmove-up)
-(global-set-key (kbd "C-x <down>") 'windmove-down)
-
-(provide 'window-manager)
