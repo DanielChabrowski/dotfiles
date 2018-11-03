20c328780dc5f5c39a769144d45e8880473b6803
Daniel Chabrowski <dantezstudio@gmail.com>
2018-11-03 16:44:23 +0100

    Add qtcreator snippets

--- qt-creator/install.sh
+++ qt-creator/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-config_dir="/home/$USER/.config/QtProject/"
+readonly QT_CONFIG_DIR="/home/$USER/.config/QtProject"
+readonly QT_SNIPPETS_DIR="$QT_CONFIG_DIR/qtcreator/snippets"
 
-if [ -d "$config_dir" ]; then
-    cp QtCreator.ini "$config_dir"
+if [ -d "$QT_CONFIG_DIR" ]; then
+    cp QtCreator.ini "$QT_CONFIG_DIR"
+    mkdir -p "$QT_SNIPPETS_DIR"
+    cp snippets.xml "$QT_SNIPPETS_DIR"
 else
     echo "QtCreator is not installed"
     exit 1
